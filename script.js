17f3c47b81a428bae0765ed2017003bc8c25fcf5
1nvoxygen <vityukha.s@gmail.com>
2025-07-04 09:25:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 script.js
-<button onclick="kupUsluge('boosting')">Kup Boosting</button>
-
-<script>
-  function kupUsluge(usluga) {
-    fetch('http://localhost:4242/create-checkout-session', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ service: usluga })
-    })
-    .then(res => res.json())
-    .then(data => window.location.href = data.url)
-    .catch(err => console.error(err));
-  }
-</script>
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Wiadomość została wysłana!");
+});
